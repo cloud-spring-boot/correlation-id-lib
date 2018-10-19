b7134028e485c6cd55063a7c1c4b46be29e4206f
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-19 11:14:36 +0300

    minor changes

--- src/main/java/com/max/correlation/UserContextHolder.java
+++ src/main/java/com/max/correlation/UserContextHolder.java
@@ -8,19 +8,21 @@ public final class UserContextHolder {
 
     private static final ThreadLocal<UserContext> CONTEXT_HOLDER = new ThreadLocal<>();
 
+    private static final String CORRELATION_ID_MDC_KEY = "correlation-id";
+
     private UserContextHolder() {
     }
 
     public static void setUserContext(UserContext userContext) {
         checkArgument(userContext != null, "null 'userContext' passed");
         CONTEXT_HOLDER.set(userContext);
-        MDC.put("correlation-id", userContext.getCorrelationId());
+        MDC.put(CORRELATION_ID_MDC_KEY, userContext.getCorrelationId());
     }
 
     public static UserContext getUserContext() {
         if (CONTEXT_HOLDER.get() == null) {
             CONTEXT_HOLDER.set(new UserContext("<undefined>"));
-            MDC.put("correlation-id", "<undefined>");
+            MDC.put(CORRELATION_ID_MDC_KEY, "<undefined>");
         }
 
         return CONTEXT_HOLDER.get();
